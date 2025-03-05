014a2cc76a7253a70c1afa433b6b15b2517b2391
kana sasazawa <kana.yellow7@gmail.com>
2025-03-06 00:22:12 +0900

    resolved merge conflict

---
+++ src/jp/co/Person2/Person.java
@@ -0,0 +1,16 @@
+package jp.co.Person2;
+
+public class Person {
+	public String name = null;
+	public int age = 0;
+	
+	//コンストラクタ１
+	public Person(){}
+	
+	//コンストラクタ２
+	public Person (String name,int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+}
