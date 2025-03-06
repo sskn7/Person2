3912e3fe8862e7b963535785061560f1da23a474
kana sasazawa <kana.yellow7@gmail.com>
2025-03-06 21:45:08 +0900

    演習３エラーを解消

--- src/jp/co/Person2/Test.java
+++ src/jp/co/Person2/Test.java
@@ -9,7 +9,7 @@ public class Test {
 		System.out.println(taro.name);
 		System.out.println(taro.age);
 		
-		Person.jiro = new Person("jiro",20);
+		Person jiro = new Person("jiro",20);
 		
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
